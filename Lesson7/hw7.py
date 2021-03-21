9f3f9b34a4af349cdf0d36f8819b1c0262896fc2
Yuankai(William) He <williamhecoin@yahoo.com>
2021-03-21 11:33:53 -0400

    Lesson 7 Initial
    Without answer

---
+++ Lesson7/hw7.py
@@ -0,0 +1,16 @@
+import requests, os, bs4, webbrowser, sys
+
+
+# 1.1 Extract the title
+
+# 1.2 Extract the author
+
+# 2.1 Extract and print all of the Lesson Links that look like "github.com......"
+
+# 2.2 Open all 4 links in a web browser
+
+# 2.3 Extract and print only Lesson 5 link
+
+# 3.1 Download all of the images
+
+# 3.2 Download the images with class="test"
